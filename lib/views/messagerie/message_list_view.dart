d1787115512298f788d2afa73ca721200bff9725
KodsLamouchi322 <159626906+KodsLamouchi322@users.noreply.github.com>
2026-04-02 09:30:00 +0100

    feat: implement message_list_view with chat_bubble integration

---
+++ lib/views/messagerie/message_list_view.dart
@@ -0,0 +1,25 @@
+﻿import 'package:flutter/material.dart';
+import '../../models/message.dart';
+import '../../widgets/chat_bubble.dart';
+
+class MessageListView extends StatelessWidget {
+  final List<Message> messages;
+  final String currentUserId;
+
+  const MessageListView({Key? key, required this.messages, required this.currentUserId}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      reverse: true,
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        final msg = messages[index];
+        return ChatBubble(
+          message: msg,
+          isMe: msg.expediteurId == currentUserId,
+        );
+      },
+    );
+  }
+}
